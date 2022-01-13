f272a5e2189026d9d4aebc16375074a6b11b93d8
zeyadsleem <zeyadsleem1@gmail.com>
2022-01-13 11:06:17 +0200

    second commet

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Main() {
   return (
     <div className="main">
-      <h1>Main</h1>
+      <h1>title</h1>
+      <p>sub title</p>
     </div>
   );
 }
